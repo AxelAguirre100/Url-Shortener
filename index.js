0bc8e1baa84f115d4333441cd85ac7dc56880a1c
Axel Aguirre <axelentoo@gmail.com>
2024-04-30 10:09:31 -0300

    primeraView

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express"
+const app = express()
+
+app.set("view engine", "ejs")
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.listen(process.env.PORT || 5000)
